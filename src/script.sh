df5200f247cd68749acd09388c6332ba74188c89
Ubuntu <ubuntu@ip-172-31-24-187.us-west-2.compute.internal>
2020-02-18 01:26:55 +0000

    src script added

--- src/script.sh
+++ src/script.sh
@@ -3,7 +3,5 @@
 N_CENTROIDS=300
 KMEANS_PKL=s3a://bucket_name/kmeans_${N_CENTROIDS}.pkl
 
-echo $KMEANS_PKL
-
-# spark-submit $SPARK_HOST:$PORT process_ingest.py
-# python3 train_kmeans.py $N_CENTROIDS $KMEANS_PKL
+spark-submit $SPARK_HOST:$PORT process_ingest.py
+python3 train_kmeans.py $N_CENTROIDS $KMEANS_PKL
